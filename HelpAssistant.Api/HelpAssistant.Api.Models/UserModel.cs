02db1852afc0f9edb2ce356121a157664caf752e
mibrahimelferga <mibrahim_elferga@hotmail.com>
2018-11-17 20:16:40 +0200

    Add initial user model in models project

---
+++ HelpAssistant.Api/HelpAssistant.Api.Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpAssistant.Api.Models
+{
+    public class UserModel
+    {
+        public long UserID { get; set; }
+        public string UserName { get; set; }
+    }
+}
