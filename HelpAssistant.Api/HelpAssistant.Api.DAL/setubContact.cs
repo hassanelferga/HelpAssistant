866bb0542542cc999770adb1b33b1091c7432be1
mibrahimelferga <mibrahim_elferga@hotmail.com>
2018-11-23 21:48:10 +0200

    Updating the sql server connection string using the shared server

---
+++ HelpAssistant.Api/HelpAssistant.Api.DAL/setubContact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HelpAssistant.Api.DAL
+{
+   public static class setubContact
+    {
+        //public static long Emergency(EmergancyModel)
+    }
+}
---
+++ HelpAssistant.Api/HelpAssistant.Api.Models/EmergencyModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpAssistant.Api.Models
+{
+    class EmergencyModel
+    
+    {
+        public int EmergencyId { get; set; }
+       public string PhoneNumber { get; set; }
+        public string ContactSMS { get; set; }
+    }
+}
