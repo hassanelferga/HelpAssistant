cf39a54d767d54b13801ce95c704dd16564ceceb
ahmed1619 <ahmad_omar9@yahoo.com>
2018-12-08 15:17:15 +0200

    Trying To Code Forget Password Method wish to Complete

---
+++ HelpAssistant.Api/HelpAssistant.Api.DAL/ForgetPassword.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HelpAssistant.Api.Models;
+
+namespace HelpAssistant.Api.DAL
+{
+   public class ForgetPassword
+    {
+        /*
+
+        public static ForgetPassswordModel ForgotPassword(string Email)
+        {
+         //   string Email = "";
+       try
+            {
+                using (SqlConnection connection = new SqlConnection(AppSetings.DbConnectionString))
+                {
+                    SqlCommand command = new SqlCommand();
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.CommandText = "SP_ForgotPassword";
+                    command.Connection = connection;
+
+                    // Add Store Procedure Paramters
+                   
+                    // Open Connection
+                    connection.Open();
+
+                    // Insert Record to the database
+                    int noOfRows = command.ExecuteNonQuery();
+                }
+}
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+            return Email;
+        }
+
+
+        public static ForgetPassswordModel UpdatePassword(string code ,string NewPssword)
+        {
+               string code = "";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(AppSetings.DbConnectionString))
+                {
+                    SqlCommand command = new SqlCommand();
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.CommandText = "SP_ForgotPassword";
+                    command.Connection = connection;
+
+                    // Add Store Procedure Paramters
+
+                    // Open Connection
+                    connection.Open();
+
+                    // Insert Record to the database
+                    int noOfRows = command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+            return code;
+        }
+
+
+    */
+    }
+   
+}
---
+++ HelpAssistant.Api/HelpAssistant.Api.Models/ForgetPassswordModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpAssistant.Api.Models
+{
+    public class ForgetPassswordModel
+    {
+        public string Code { get; set; }
+        public bool StatusCode { get; set; }
+        public string ErrorMsg { get; set; }
+
+
+    }
+}
